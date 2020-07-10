213b2d4142a7c12141379359bf55c71c766f8223
Ashley Allphin <ashley.allphin@gmail.com>
2020-07-10 15:36:08 -0400

    setting up server.js file how i like it, getting ready to use morgan, connecting to mongodb atlas cloud successfully -- ready to test connection with postman

--- server.js
+++ server.js
@@ -1,37 +1,46 @@
+// =========================
+const color = require('colors-cli/toxic');
+const morgan = require ("morgan");
 const express = require("express");
-const app = express();
 const routes = require("./routes")
+const path = require('path');
+const dotenv = require ('dotenv');
+dotenv.config();
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//connect to mongo
+// =========================
 const mongoose = require("mongoose");
   //to circumvent deprecation warnings
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useUnifiedTopology', true);
     mongoose.set('useCreateIndex', true);
-// If deployed, use the deployed database. Otherwise, use the local 'ringerHeadlines' database.
-var db = process.env.MONGODB_URI || "mongodb://localhost/ringerHeadlines";
 
-//tell mongoose to connect to the database
-mongoose.connect(db, function(error) {
-    if (error) {
-    //log any errors to the console
-    console.log("There was an error: + " + error);
-    }
-    else {
-    //or log a success message to the console
-    console.log("Mongoose connection created successfully.");
-    }
-});
+var db = process.env.MONGODB_URI
+mongoose.connect(db)
+.then(() => console.log(`Successfully connected to MongoDB.`.x204 + 
+`\n+++++++++++++++++++++++++++++++++++++++++\n`.x141))
+mongoose.connection.on('error', err =>
+console.log(`Error connectiong to MongoDB: ${err.message}`.x196));
 
+
+// =========================
+const app = express();
 app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+    
 
-app.listen(PORT, function() {
-    console.log(`App listening on PORT https://localhost${PORT}`);
+// =========================
+app.use(morgan('dev'));
+
+
+// =========================
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log(`\n+++++++++++++++++++++++++++++++++++++++++`.x141 + `\nApp listening on `.x154 + `http://localhost:${PORT}`.x43.underline);
 });
 
